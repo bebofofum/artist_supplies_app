d92dd2d75ab151336ba1e6256f58a6b48328999c
bebofofum <bri.boehm@gmail.com>
2020-09-02 23:13:27 -0400

    Added a config folder with environment.rb file

---
+++ config/environment.rb
@@ -0,0 +1,14 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
+
+Dotenv.load
+
+require './app/controllers/application_controller'
+
+require_all 'app'
+
+
