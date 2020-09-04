02e2f3a6ffcdbf5e0b0241b666d9974e346659d4
bebofofum <bri.boehm@gmail.com>
2020-09-04 14:18:18 -0400

    Added configure do set :public_folder, :views, :sessions, :session_secret, amd :method_override and private helper methods current_user and logged_in?

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    set :sessions, true
+    set :session_secret, ENV["SESSION_SECRET"]
+    set :method_override, true
+  end
+
+  
+
+  private 
+
+  def current_user 
+    User.find_by_id(session[:id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+end
