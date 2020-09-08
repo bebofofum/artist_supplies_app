491fcf146e5e8e8f63eefee2b2294b82dd46194c
bebofofum <bri.boehm@gmail.com>
2020-09-08 13:27:52 -0400

    Adjusted has_many naming to work with camelcase naming of ArtItem class. Sep name with underscore

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :artitems, foreign_key: "author_id"
+  has_many :art_items, foreign_key: "author_id"
 
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
